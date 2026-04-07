90db2af13c528575fc86621aba6b3bd9a93bff99
codecutbuildx-blip <254323929+codecutbuildx-blip@users.noreply.github.com>
2026-04-07 10:49:04 +0000

    Add python tutorial [Apr 07, 2026]

---
+++ tutorials/2026/04/python_07_apr_973.py
@@ -0,0 +1,10 @@
+# Trie Data Structure in Python
+=====================================
+
+## Introduction
+---------------
+
+A Trie is a prefix tree data structure that stores a collection of strings in a way that allows for efficient retrieval and manipulation of words.
+
+## Implementation
+-----------------
