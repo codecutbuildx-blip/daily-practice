d8ebf634b2167933105c8199d1b814790784b292
codecutbuildx-blip <254323929+codecutbuildx-blip@users.noreply.github.com>
2026-04-08 12:26:33 +0000

    Add python tutorial [Apr 08, 2026]

---
+++ tutorials/2026/04/python_08_apr_544.py
@@ -0,0 +1,10 @@
+# Segment Tree Implementation in Python
+====================================================
+
+## Introduction
+------------
+
+A segment tree is a data structure used for range queries and updates on arrays. It's particularly useful when you need to query or update multiple elements within a given range.
+
+## Code
+-----
